94a99b684cc7eee05dcec2c5fa53d5e5c2f10c08
GrebinaLena <ne_hochu@mail.ru>
2018-04-16 22:05:35 +0400

    добавлены методы: Sub, Mul, Div, Pow; изменена Main

--- ITUniver.TeleCalc.ConCalc/Calc.cs
+++ ITUniver.TeleCalc.ConCalc/Calc.cs
@@ -8,9 +8,40 @@ namespace ITUniver.TeleCalc.ConCalc
 {
     public class Calc
     {
-        public int Sum(int x, int y)
+        public double Sum(double x, double y)
         {
             return x+y;
-        } 
+        }
+
+        public double Sub(double x, double y)
+        {
+            return x-y;
+        }
+
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Div(double x, double y)
+        {
+            if (y != 0)
+            {
+                return x / y;
+            }
+            else
+            {
+                Console.WriteLine("Деление на ноль невозможно!");
+                return 0;
+            }
+            
+        }
+
+        public double Pow(double x, double y)
+        {
+           return Math.Pow(x, y);
+
+        }
+
     }
 }
--- ITUniver.TeleCalc.ConCalc/Program.cs
+++ ITUniver.TeleCalc.ConCalc/Program.cs
@@ -10,6 +10,52 @@ namespace ITUniver.TeleCalc.ConCalc
     {
         static void Main(string[] args)
         {
+            Calc Calculator = new Calc();
+            double x;
+            double y;
+            bool args1 = (Double.TryParse(args[1], out x));
+            bool args2 = (Double.TryParse(args[2], out y));
+
+            if ((args1 == true) && (args2 == true))
+            {
+                if (args[0].ToLower() == "sum")
+                {
+                    Console.WriteLine(Calculator.Sum(x, y));
+                }
+
+                if (args[0].ToLower() == "sub")
+                {
+                    Console.WriteLine(Calculator.Sub(x, y));
+                }
+
+                if (args[0].ToLower() == "mul")
+                {
+                    Console.WriteLine(Calculator.Mul(x, y));
+                }
+
+                if (args[0].ToLower() == "div")
+                {
+                    Console.WriteLine(Calculator.Div(x, y));
+                }
+
+                if (args[0].ToLower() == "pow")
+                {
+                    Console.WriteLine(Calculator.Pow(x, y));
+                }
+                else
+                {
+                    Console.WriteLine("Операция не существует");
+                }
+            }
+
+           else
+            {
+                Console.WriteLine("Операция не может быть выполнена");
+            }
+
+            Console.ReadLine();
+
         }
+
     }
 }
