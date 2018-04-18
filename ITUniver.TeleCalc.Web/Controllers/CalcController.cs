1162b6b50a0d104591d9a00fe03da2bd6c92b489
GrebinaLena <ne_hochu@mail.ru>
2018-04-18 23:46:05 +0400

    Добавлено выведение списка операций, выичисление операций в контроллере методом Exec()

--- ITUniver.TeleCalc.Web/Controllers/CalcController.cs
+++ ITUniver.TeleCalc.Web/Controllers/CalcController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ITUniver.TeleCalc.Core.Operations;
+using ITUniver.TeleCalc.Core;
 
 
 namespace ITUniver.TeleCalc.Web.Controllers
@@ -11,18 +12,20 @@ namespace ITUniver.TeleCalc.Web.Controllers
     public class CalcController : Controller
     {
         // GET: Calc
+        Calc Calculator = new Core.Calc();
         [HttpGet]
         public ActionResult Index(string operName, double? x, double? y)
         {
             ViewBag.OperName = operName;
             ViewBag.x = x;
             ViewBag.y = y;
+            ViewBag.Result = Calculator.Exec(ViewBag.OperName, ViewBag.x, ViewBag.y);
             return View();
         }
 
         public ActionResult Operations()
         {
-            ViewBag.operations = new Core.Calc().GetOperNames();
+            ViewBag.operations = Calculator.GetOperNames();
             return View();
         }
     }
