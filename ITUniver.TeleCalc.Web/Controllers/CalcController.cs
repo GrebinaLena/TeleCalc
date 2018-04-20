4f447cc273fcd2454a78caa052c94fc7f891c994
GrebinaLena <ne_hochu@mail.ru>
2018-04-20 10:07:59 +0400

    модель

--- ITUniver.TeleCalc.Web/Controllers/CalcController.cs
+++ ITUniver.TeleCalc.Web/Controllers/CalcController.cs
@@ -5,27 +5,57 @@ using System.Web;
 using System.Web.Mvc;
 using ITUniver.TeleCalc.Core.Operations;
 using ITUniver.TeleCalc.Core;
-
+using ITUniver.TeleCalc.Web.Models;
 
 namespace ITUniver.TeleCalc.Web.Controllers
 {
     public class CalcController : Controller
     {
         // GET: Calc
-        Calc Calculator = new Core.Calc();
+        Calc Calc = new Core.Calc();
+
+
+        [HttpGet]
+        public ActionResult Exec()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+
+        public ActionResult Exec(CalcModel model)
+        {
+            if (Calc.GetOperNames().Contains(model.OperName))
+            {
+                model.Result = Calc.Exec(model.OperName, model.X, model.Y);
+            }
+            return View(model);
+        }
+
+
         [HttpGet]
         public ActionResult Index(string operName, double? x, double? y)
         {
-            ViewBag.OperName = operName;
-            ViewBag.x = x;
-            ViewBag.y = y;
-            ViewBag.Result = Calculator.Exec(ViewBag.OperName, ViewBag.x, ViewBag.y);
+            if (Calc.GetOperNames().Contains(operName))
+            {
+                ViewBag.OperName = operName;
+                ViewBag.x = x;
+                ViewBag.y = y;
+                ViewBag.Result = Calc.Exec(operName, x ?? 0, y ?? 0);
+            }
+            else
+            {
+                ViewBag.Error = "Укажите операцию";
+            }
+
             return View();
         }
 
         public ActionResult Operations()
         {
-            ViewBag.operations = Calculator.GetOperNames();
+
+            ViewBag.operations = Calc.GetOperNames();
             return View();
         }
     }
---
+++ ITUniver.TeleCalc.Web/Models/CalcModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITUniver.TeleCalc.Web.Models
+{
+    public class CalcModel
+    {
+        public string OperName { get; set; }
+
+        public double X { get; set; }
+
+        [Required(ErrorMessage = "Пропустил!")]
+        public double Y { get; set; }
+
+        [ReadOnly(true)]
+        public double Result { get; set; }
+
+    }
+}
